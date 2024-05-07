3b9660b858ac6ef358eff801a476085055299665
Mark Anthony Sol Cruz <solcruzmarkanthony@gmail.com>
2024-05-07 15:03:03 +0800

    api https

--- js/mailer.js
+++ js/mailer.js
@@ -12,7 +12,7 @@ document.querySelector('form').addEventListener('submit', function (event) {
     queryParams.append('email', email);
     queryParams.append('message', message);
 
-    const url = `http://192.168.1.28:8080/test-landia/web/api/contact-us?${queryParams.toString()}`;
+    const url = `https://aep.e-cmv.ph/api/contact-us?${queryParams.toString()}`;
 
     fetch(url, {
         method: 'GET',
